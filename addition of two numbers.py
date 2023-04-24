7b453eaa6339b2834da7c1ba034d3d8800add882
ezeamii <131669112+ezeamii@users.noreply.github.com>
2023-04-24 13:00:06 +0100

    Add files via upload

---
+++ addition of two numbers.py
@@ -0,0 +1,6 @@
+# code to request and add two numbers
+a=input("Put in your first number: ")
+b=input("Put in your second number: ")
+
+sum = float(a)+float(b)
+print("The sum of both numbers is", sum)
